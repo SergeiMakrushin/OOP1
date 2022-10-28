110420bcf53ad53c68a3c502b27e44a46f4b45e8
Сергей <mak.sergei@ngs.ru>
2022-10-28 13:55:12 +0700

    HW

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
+import transport.Car;
+import transport.Car.Key;
+import transport.Car.Insurance;
+import transport.Car.Toy;
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Key king=new Key  ("да", "нет");
+        Insurance valeo = new Insurance("2022-09-12", 5,"tyrf");
+        Toy toy = new Toy(42, 54);
+
         Car lada = new Car(
                 "Lada",
                 "Granta",
                 2015,
                 "России",
                 "жёлтый",
-                1.7f);
+                1.7f,
+                "автоматическая", "x000xx000",
+                "седан",
+
+                3, "летние");
+       lada.setKey(king);
 
         Car audi = new Car(
                 "Audi",
@@ -15,15 +31,23 @@ public class Main {
                 2020,
                 "Германии",
                 "черный",
-                3.0f);
-
+                3.0f,
+                "механическа","x000xx000" ,
+                "седан",
+                4,
+                "зимние");
+        //х000хх000
         Car bmv = new Car(
                 "BMW",
                 "Z8",
                 2021,
                 "Германии",
                 "черный",
-                3.0f);
+                3.0f,
+                "робот","x000xx000" ,
+                "купе",
+
+                3, "летние");
 
         Car kia = new Car(
                 "Kia",
@@ -31,7 +55,10 @@ public class Main {
                 2018,
                 "Южной Корее",
                 "красный",
-                2.4f);
+                2.4f,
+                "автоматическа","x000xx000" ,
+                "кроссовер",
+                5, "летние");
 
         Car hyundai = new Car(
                 "Hyundai",
@@ -39,14 +66,32 @@ public class Main {
                 2016,
                 "Южной Корее",
                 "оранжевый",
-                1.6f);
+                1.6f,
+                "механическая","x000xx000" ,
+                "седан",
 
+                4, "зимние");
+
+
+hyundai.changeTyres();
+kia.changeTyres();
+        hyundai.setRegistrationNumber("gfrg");
+        hyundai.number();
         lada.Print();
         audi.Print();
         bmv.Print();
         kia.Print();
         hyundai.Print();
+            hyundai.setKey(king);
+            hyundai.setInsurance(valeo);
+            hyundai.setToy(toy);
 
-
-    }
+        System.out.println("hyundai.key = " + hyundai.key);
+        System.out.println("hyundai.insurance = " + hyundai.insurance);
+            System.out.println("hyundai.getInsurance() = " + hyundai.getInsurance().getCostInsurance());
+            System.out.println("hyundai.getToy() = " + hyundai.getToy());
+            System.out.println("toy = " + toy);
+            System.out.println("toy.getB() = " + toy.getB());
+            System.out.println("valeo.getCostInsurance() = " + valeo.getCostInsurance());
+        }
 }
---
+++ transport/Car.java
@@ -0,0 +1,336 @@
+package transport;
+
+import java.time.LocalDate;
+
+public class Car {
+
+    public static class Toy {
+        private int e;
+       private int b;
+
+
+        public Toy(int e, int b) {
+            this.e = e;
+            this.b = b;
+        }
+
+        public int getE() {
+            return e;
+        }
+
+        public void setE(int e) {
+            this.e = e;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
+    }
+
+    public static class Key {
+        private String remoteEngineStart;
+        private String keyFree;
+
+        public Key(String remoteEngineStart, String keyFree) {
+            if (remoteEngineStart == null && remoteEngineStart.isBlank()) {
+                this.remoteEngineStart = remoteEngineStart;
+            }
+            if (keyFree != null && !keyFree.isBlank()) {
+                if (keyFree.equalsIgnoreCase("есть") || keyFree.equalsIgnoreCase("нет")) {
+                    this.keyFree = remoteEngineStart;
+                } else this.keyFree = "Данные неверны";
+            }
+        }
+
+        public String getRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public String getKeyFree() {
+            return keyFree;
+        }
+
+    }
+
+    //- Срок действия страховки
+//- Стоимость страховки
+//- Номер страховки
+    public static class Insurance {
+        private String insurancePeriod;
+        private double costInsurance;
+        private String numberInsurance;
+        double s = Math.pow(10, 3);
+        LocalDate b;
+
+        public Insurance(String insurancePeriod, double costInsurance, String numberInsurance) {
+          //  this.insurancePeriod = insurancePeriod;
+            if (this.insurancePeriod == null || this.insurancePeriod.isBlank() || this.insurancePeriod.length() != 10) {
+                System.out.println("Заполните данные в формате гггг-мм-дд  с тире");
+            } else if (Character.isDefined(this.insurancePeriod.charAt(4))
+                    || Character.isDefined(this.insurancePeriod.charAt(7))
+                    || Character.isDigit(this.insurancePeriod.charAt(0))
+                    || Character.isDigit(this.insurancePeriod.charAt(1))
+                    || Character.isDigit(this.insurancePeriod.charAt(2))
+                    || Character.isDigit(this.insurancePeriod.charAt(3))
+                    || Character.isDigit(this.insurancePeriod.charAt(5))
+                    || Character.isDigit(this.insurancePeriod.charAt(6))
+                    || Character.isDigit(this.insurancePeriod.charAt(8))
+                    || Character.isDigit(this.insurancePeriod.charAt(9))) {
+                LocalDate b = LocalDate.parse(this.insurancePeriod);
+                b.isBefore(LocalDate.now());
+                System.out.println("Поменяйте страховку");
+            }
+            if (costInsurance < 0) {
+                costInsurance = Math.abs(costInsurance);
+                this.costInsurance = Math.ceil(costInsurance * s) / s;
+            }
+            if (numberInsurance == null || numberInsurance.isBlank()) {
+                if (numberInsurance.length() != 9) {
+                    this.numberInsurance = "Неверный номер";
+                } else this.numberInsurance = numberInsurance;
+            }
+        }
+
+        public String getNumberInsurance() {
+            return numberInsurance;
+        }
+
+        public String getInsurancePeriod() {
+
+            return insurancePeriod;
+        }
+
+        public double getCostInsurance() {
+            return costInsurance;
+        }
+    }
+
+    private final String brand;
+    private final String model;
+    private float engineVolume;
+    private String color;
+    private final int productionYear;
+    private final String productionCountry;
+    private String transmission;
+    private final String body;
+    private final int numberSeats;
+    private String tyres;
+    String registrationNumber;
+    public Key key;
+    public Insurance insurance;
+    private Toy toy;
+
+
+
+
+    public Car(String brand, String model, int productionYear, String productionCountry, String color,
+               float engineVolume,
+               String transmission, String registrationNumber, String body, int numberSeats, String tyres) {
+        if (brand == null || model == null || productionCountry == null) {
+            this.brand = "default";
+            this.model = "default";
+            this.productionCountry = "default";
+        } else {
+            this.brand = brand;
+            this.model = model;
+            this.productionCountry = productionCountry;
+        }
+
+        if (Float.compare(engineVolume, 0) == 0) {
+
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isBlank()) {
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        if (transmission != null || !transmission.isEmpty() || !transmission.isBlank()) {
+            this.transmission = transmission;
+        } else {
+            this.transmission = ": Информация не указана";
+        }
+
+        if (body != null || !body.isEmpty() || !body.isBlank()) {
+            this.body = body;
+        } else {
+            this.body = ": Информация не указана";
+        }
+
+        if (registrationNumber != null || registrationNumber.isBlank()) {
+
+            this.registrationNumber = registrationNumber;
+
+        } else {
+            this.registrationNumber = "Неправильный номер";
+        }
+
+        this.numberSeats = numberSeats;
+
+        if (tyres != null || !tyres.isEmpty() || !tyres.isBlank()) {
+            this.tyres = tyres;
+        } else {
+            this.tyres = ": Информация не указана";
+        }
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+
+    public void setKey(Key key) {
+        if (key == null) {
+            this.key = key;
+        }
+    }
+    public Toy getToy() {
+        return toy;
+    }
+
+    public void setToy(Toy toy) {
+        this.toy = toy;
+    }
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getNumberSeats() {
+        return numberSeats;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getTires() {
+        return tyres;
+    }
+
+    public void setTires(String tires) {
+        this.tyres = tires;
+    }
+
+    public void Print() {
+        System.out.println(
+                brand + " "
+                        + model + ", "
+                        + productionYear
+                        + " год выпуска, сборка в "
+                        + productionCountry
+                        + " , цвет "
+                        + color
+                        + " , объём двигателя "
+                        + engineVolume + " л, коробка передач "
+                        + transmission + ", тип кузова "
+                        + body + ", регистрационный номер: " + registrationNumber + ", количество мест "
+                        + numberSeats + ", шины "
+                        + tyres + ".");
+    }
+
+    public void Keys() {
+        System.out.println(getKey());
+    }
+
+    // public Key(String remoteEngineStart, String keyFree) {
+//            this.remoteEngineStart = remoteEngineStart;
+//            this.keyFree = keyFree;
+//        }
+    public void Kprint() {
+        System.out.println(getInsurance());
+
+    }
+
+    public void changeTyres() {
+        String a = "летние";
+        if (tyres.equals(a) == true) {
+            tyres = "зимние";
+        } else {
+            tyres = "летние";
+        }
+    }
+
+    public void number() {
+        this.registrationNumber.toCharArray();
+        if (this.registrationNumber.toCharArray().length != 9) {
+            this.registrationNumber = "Неверная длина номера";
+        } else if (!Character.isLetter(this.registrationNumber.charAt(0))
+                || !Character.isLetter(this.registrationNumber.charAt(4))
+                || !Character.isLetter(this.registrationNumber.charAt(5))) {
+            this.registrationNumber = "Неверно указан символ";
+        } else if (!Character.isDigit(this.registrationNumber.charAt(1))
+                || !Character.isDigit(this.registrationNumber.charAt(2))
+                || !Character.isDigit(this.registrationNumber.charAt(3))
+                || !Character.isDigit(this.registrationNumber.charAt(6))
+                || !Character.isDigit(this.registrationNumber.charAt(7))
+                || !Character.isDigit(this.registrationNumber.charAt(8))) {
+            this.registrationNumber = "Неверно указан символ!";
+        } else {
+            this.registrationNumber = registrationNumber.toLowerCase();
+        }
+    }
+}
